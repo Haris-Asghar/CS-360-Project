5106718f75c998dc81f19bc6709ae55df5cd885e
Haris-Asghar <harisasghar9999@gmail.com>
2024-03-02 04:18:01 +0500

    Power users added

--- server/src/index.js
+++ server/src/index.js
@@ -2,17 +2,19 @@ import express from "express";
 import cors from "cors"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
-dotenv.config()
 import {employeeRouter} from "./routes/employee.js"
+
+dotenv.config()
 
 const app = express();
 const username = process.env.DB_USER_NAME;
 const password = process.env.DB_PASSWORD;
+const dbName = process.env.DB_CLUSTER_NAME;
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(`mongodb+srv://${username}:${password}@cs360.c0zyvyg.mongodb.net/cs360?retryWrites=true&w=majority&appName=cs360`)
+mongoose.connect(`mongodb+srv://${username}:${password}@${dbName}.c0zyvyg.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=${dbName}`)
 
 app.use("/auth", employeeRouter);
 
--- server/src/routes/employee.js
+++ server/src/routes/employee.js
@@ -1,7 +1,6 @@
 import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-
 import {EmployeeModel} from "../models/Employee.js";
 
 const router = express.Router(); 
@@ -15,26 +14,22 @@ router.post("/register", async (req, res) => {
     const hash = await bcrypt.hash(password, 10);
     const newEmployee = new EmployeeModel({role, username, fname, lname, salary, email, pnumber, password: hash});
     await newEmployee.save();
-
     res.status(200).send("User registered");
 });
 
 router.post("/login", async (req, res) => {
     const {role, username, password} = req.body;
-
     const employee = await EmployeeModel.findOne({username});
 
     if(!employee) return res.status(400).send("Employee not found");
 
     if (role !== employee.role) return res.status(400).send("Employee not found");
-
     const match = await bcrypt.compare(password, employee.password);
-    if(!match) return res.status(400).send("Wrong password");
 
+    if(!match) return res.status(400).send("Wrong password");
     const token = jwt.sign({id: employee._id}, "secret");
+
     res.status(200).send({token, employeeID: employee._id, employeeRole: employee.role});
 });   
-
-
 
 export {router as employeeRouter};
