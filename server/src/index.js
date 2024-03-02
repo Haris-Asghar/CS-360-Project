01df09496ea408d3630e596dd20bfaabf27eb0d7
aadrishmahmood <118641419+aadrish7@users.noreply.github.com>
2024-03-02 17:09:32 +0500

    Added one exception handling in server file

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,10 @@
 import express from "express";
-import cors from "cors"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-import {employeeRouter} from "./routes/employee.js"
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { employeeRouter } from "./routes/employee.js";
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 const username = process.env.DB_USER_NAME;
@@ -15,7 +15,12 @@ app.use(express.json());
 app.use(cors());
 
 mongoose.connect(`mongodb+srv://${username}:${password}@${dbName}.c0zyvyg.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=${dbName}`)
+  .then(() => {
+    console.log("\x1b[34m%s\x1b[0m", "DB connected"); 
+    app.listen(3001, () => console.log("\x1b[33m%s\x1b[0m", "Listening at port 3001"));
+  })
+  .catch((err) => {
+    console.error("\x1b[31m%s\x1b[0m", "DB connection error:", err);
+  });
 
 app.use("/auth", employeeRouter);
-
-app.listen(3001, () => console.log("Listening at port 3001"))
