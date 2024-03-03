e0d638be749cf1eb3bea3a53a5622633976c530b
aadrishmahmood <118641419+aadrish7@users.noreply.github.com>
2024-03-03 13:12:46 +0500

    Integrated time and Date into Log

--- server/src/models/Attendance.js
+++ server/src/models/Attendance.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
 
 const attendanceSchema = new mongoose.Schema({
-    username: {type: String, required: true, unique:true},
-    logDate: { type: Date, default: Date.now },
-    logTime: { type: String, required: true },
+    username: { type: String, required: true },
+    log: { type: Date, default: Date.now },
     leave: { type: Boolean, default: false }
 });
 
 const AttendanceModel = mongoose.model("Attendance", attendanceSchema);
-
 export default AttendanceModel;
+
 
