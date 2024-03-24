bddb3f46bddd4ce3cd2718cdb5025a6d35982940
Haris-Asghar <harisasghar9999@gmail.com>
2024-03-24 07:57:04 +0500

    Minor changes

--- client/src/pages/Admin/Dashboard.js
+++ client/src/pages/Admin/Dashboard.js
@@ -1,14 +1,10 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import { UserContext } from '../../components/User_Context';
-import { datesFromTodayToStartOfMonth, datesMatcher } from './Utilities/dateUtils';
-import { fetchAttendanceData } from './Utilities/api';
 import PresentEmployeesTable from '../../components/Present_Employees_Table';
-import MyCalendar from '../../components/Calendar';
 import MyClock from '../../components/Clock';
 
 const AdminDashboard = () => {
     const { user } = useContext(UserContext);
-    const [markedDates, setMarkedDates] = useState([]);
 
     // Dummy data for demonstration
     const presentEmployees = [
@@ -17,28 +13,12 @@ const AdminDashboard = () => {
         { name: 'Employee 3', attendanceTime: '09:30 AM' },
     ];
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const data = await fetchAttendanceData(user.username);
-                setMarkedDates(datesMatcher(datesFromTodayToStartOfMonth(), data.attendanceRecordsThisMonth));
-            } catch (error) {
-                console.error(error);
-            }
-        };
-        fetchData();
-    }, [user.username]);
-
     return (
         <div>
             {/* <AdminNavbar /> */}
             <p>Username: {user.username}</p>
             <p>Role: {user.role}</p>
             <h1>Welcome to Employee Attendance System</h1>
-            <div className="calendar-section">
-                <h2>Calendar</h2>
-                <MyCalendar markedDates={markedDates} />
-            </div>
             <div className="clock-section">
                 <h2>Clock</h2>
                 <MyClock />
